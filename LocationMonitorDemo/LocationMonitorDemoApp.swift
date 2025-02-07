bd84a7b0d1de26e5b8aac2ff9479a9650117d545
itsuki <itsuki.enjyoy@gmail.com>
2025-02-07 14:36:23 +0900

    Initial Commit

---
+++ LocationMonitorDemo/LocationMonitorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationMonitorDemoApp.swift
+//  LocationMonitorDemo
+//
+//  Created by Itsuki on 2025/02/07.
+//
+
+import SwiftUI
+
+@main
+struct LocationMonitorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
